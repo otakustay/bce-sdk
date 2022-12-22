3a9119f91e026e9c6a2f6c99b4af7bdf9689bd69
otakustay <otakustay@gmail.com>
2022-12-22 17:32:30 +0800

    feat(bls): 支持V2版本queryLogRecord接口

--- src/bls/index.ts
+++ src/bls/index.ts
@@ -11,15 +11,41 @@ export interface LogRecordQuery {
     endDateTime: Date;
 }
 
-export interface LogRecordResultSet {
+export interface LogRecordQueryV2 extends LogRecordQuery {
+    limit?: number;
+    sort?: 'asc' | 'desc';
+}
+
+interface LogRecordResultSetBase {
     columns: string[];
-    rows: any[][];
+    isTruncated: boolean;
+    truncatedReason: string;
+}
+
+export interface LogRecordResultSet extends LogRecordResultSetBase {
+    columns: string[];
     isTruncated: boolean;
     truncatedReason: string;
 }
 
 export interface QueryLogRecordResponse {
     resultSet?: LogRecordResultSet;
+}
+
+export interface LogRecordResultItem {
+    timestamp: number;
+    stream: string;
+    tags: Array<Record<string, unknown>>;
+    offset: number;
+    message: string;
+}
+
+export interface LogRecordResultSetV2 extends LogRecordResultSetBase {
+    logRecords: LogRecordResultItem[];
+}
+
+export interface QueryLogRecordResponseV2 {
+    resultSet?: LogRecordResultSetV2;
 }
 
 export type BlsOptions = RegionClientOptions;
@@ -38,6 +64,23 @@ export class BlsClient {
             {
                 params: {
                     query,
+                    startDateTime: stringifyDate(startDateTime),
+                    endDateTime: stringifyDate(endDateTime),
+                },
+            }
+        );
+        return response;
+    }
+
+    async queryLogRecordV2({logStoreName, query, startDateTime, endDateTime, limit, sort}: LogRecordQueryV2) {
+        const response = await this.http.json<QueryLogRecordResponseV2>(
+            'GET',
+            `/v2/logstore/${logStoreName}/logrecord`,
+            {
+                params: {
+                    query,
+                    limit,
+                    sort,
                     startDateTime: stringifyDate(startDateTime),
                     endDateTime: stringifyDate(endDateTime),
                 },
